6444d006bc01c7a6ed315e1f7f901ae80dab83a7
samuelecelani <129739702+samuelecelani@users.noreply.github.com>
2026-01-19 15:53:56 +0100

    update root-app

--- build-and-deploy.ps1
+++ build-and-deploy.ps1
@@ -4,13 +4,13 @@
 
 #docker builder prune -a -f
 
-Write-Host "Costruisco l'immagine Docker per dfp-gateway..."
-docker build -t dfp-gateway:1.0.0 "./dfp-gateway"
+#Write-Host "Costruisco l'immagine Docker per dfp-gateway..."
+#docker build -t dfp-gateway:1.0.0 "./dfp-gateway"
 
 #docker builder prune -a -f
 
 Write-Host "Costruisco l'immagine Docker per dfp-piao-fe..."
-docker build -t dfp-performance-fe:1.0.0 "./dfp-piao-fe"
+docker build -t dfp-piao-fe:1.0.0 "./dfp-piao-fe"
 
 # Avvia docker-compose solo dopo il completamento delle build
 Write-Host "Avvio docker-compose..."
@@ -32,3 +32,5 @@ docker push localhost:8081/dfp-docker-repo/dfp-gateway:1.0.0
 
 
 Write-Host "Pushed"
+
+
